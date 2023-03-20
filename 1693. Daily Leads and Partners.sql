8d4c516e95ae1d14a5a6cd7ce9b5d42d29529b60
Kamron <wieeil84@gmail.com>
2023-03-20 11:42:39 +0500

    query file

---
+++ 1693. Daily Leads and Partners.sql
@@ -0,0 +1,8 @@
+create table dailysales(
+    date_id date,
+    make_name varchar,
+    lead_id int,
+    partner_id int
+);
+
+select date_id,make_name,count(distinct lead_id) as unique_leads,count(distinct partner_id) as unique_partners from DailySales group by date_id,make_name;
